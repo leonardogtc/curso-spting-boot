d62cd284b8106f049d1a28a10d7e0b7882421b2a
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-02-10 14:21:45 -0300

    Primeiro Commit

---
+++ src/main/java/io/github/cursodsousa/produtosapi/model/Produto.java
@@ -0,0 +1,51 @@
+package io.github.cursodsousa.produtosapi.model;
+
+// POJO - Plain Old Java Object
+public class Produto {
+    private String id;
+    private String nome;
+    private String descricao;
+    private String preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco='" + preco + '\'' +
+                '}';
+    }
+}
